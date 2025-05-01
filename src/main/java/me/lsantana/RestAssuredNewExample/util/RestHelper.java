29c868f75f1098cf30764adb0ef20c348f2f4448
Leandro <leandro.rodrigues.santana@hotmail.com>
2025-05-01 13:14:37 -0300

    Example of a test project using RestAssured with testng

---
+++ src/main/java/me/lsantana/RestAssuredNewExample/util/RestHelper.java
@@ -0,0 +1,15 @@
+package me.lsantana.RestAssuredNewExample.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RestHelper {
+	
+	ObjectMapper mapper = new ObjectMapper();
+	
+	public String createRequest(Object obj) throws JsonProcessingException {
+		String jsonInString = mapper.writeValueAsString(obj);
+		return jsonInString;
+	}
+
+}
---
+++ src/test/java/me/lsantana/RestAssuredNewExample/test/ExampleClassTest.java
@@ -0,0 +1,94 @@
+package me.lsantana.RestAssuredNewExample.test;
+
+import static io.restassured.RestAssured.*;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import me.lsantana.RestAssuredNewExample.util.GenerateReport;
+import me.lsantana.RestAssuredNewExample.util.RestHelper;
+
+public class ExampleClassTest extends GenerateReport {
+
+	Response response;
+	RestHelper restHelper = new RestHelper();
+
+	Map<String, Object> bodyMap = new HashMap<String, Object>();
+
+	List<Header> headerList = new ArrayList<Header>();
+
+	@DataProvider(name = "dataPostUser")
+	public Object[][] dataPostUser() {
+		return new Object[][] {
+			{"", "123456"},
+			{"newUser", ""},
+		};
+	}
+
+	@BeforeMethod
+	public void set() {
+		RestAssured.baseURI = "https://quickpizza.grafana.com";
+		bodyMap.clear();
+	}
+
+	@AfterMethod
+	public void finish() {
+		afterAPITest(bodyMap, response);
+	}
+
+	@Test
+	public void getMethodTest() {
+		response = when().get("/");
+
+		Assert.assertEquals(response.getHeader("Content-Type"), "text/html; charset=utf-8");
+		Assert.assertEquals(response.statusCode(), 200);
+	}
+
+	@Test
+	public void createUserTest() {
+		bodyMap.put("username", "newUser");
+		bodyMap.put("password", "12345678");
+
+		response = given()
+				.contentType(ContentType.JSON)
+				.body(bodyMap)
+				.when()
+				.post("/api/users");
+
+
+		Assert.assertEquals(response.statusCode(), 201);
+		Assert.assertEquals(response.getHeader("Content-Type"), "application/json");
+		Assert.assertNotEquals(response.getBody(), null);
+		Assert.assertEquals(response.jsonPath().getString("username"), "newUser");
+	}
+
+	@Test(dataProvider = "dataPostUser")
+	public void createInvalidUserTest(String username, String password) {
+		bodyMap.put("username", username);
+		bodyMap.put("password", password);
+
+		response = given()
+				.contentType(ContentType.JSON)
+				.body(bodyMap)
+				.when()
+				.post("/api/users");
+
+
+		Assert.assertEquals(response.statusCode(), 400);
+		Assert.assertEquals(response.getHeader("Content-Type"), "application/json");
+		Assert.assertNotEquals(response.getBody(), null);
+		Assert.assertNotEquals(response.jsonPath().getString("error"), null);
+	}
+}
